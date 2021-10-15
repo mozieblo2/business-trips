09a3dab8572c2dd9569d30491fc115ecf62bc575
Magdalena Oziębło <magdalena.ozieblo@itechart-group.com>
2021-10-15 09:52:17 +0200

    Material ui basic settings

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { IntlProvider } from 'react-intl';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { theme } from 'styles/theme';
 import App from './App';
 import { messages, appLang } from './translations';
-import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <IntlProvider locale={appLang} messages={messages[appLang]}>
-            <App />
-        </IntlProvider>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <IntlProvider locale={appLang} messages={messages[appLang]}>
+                <App />
+            </IntlProvider>
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
